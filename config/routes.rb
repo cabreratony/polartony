c5b0d37ee831a98c1fda5db9ab39f110fa84bd82
Juan Rayas <rayasgera2994@gmail.com>
2017-08-28 16:09:36 -0500

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  root to: 'discussions#index'
+  resources :discussions
+
+  get '/auth/:provider/callback' => 'sessions#new'
+  get '/auth/sign-out' => 'sessions#sign_out', as: :sign_out
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
